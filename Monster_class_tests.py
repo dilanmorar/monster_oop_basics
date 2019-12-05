dca2edf4908db12ef680837d751d82b4b8581840
Dilan Morar <DMorar@spartaglobal.com>
2019-12-05 21:50:47 +0000

    tests run

---
+++ Monster_class_tests.py
@@ -0,0 +1,9 @@
+from Monster_class import *
+
+monster1 = Monster('Sully', 'Purple and blue', 'strong')
+print('Check to see if monster makes a noise during eating: ')
+print(monster1.eat())
+print('Check to see if monster makes a noise during sleeping:')
+print(monster1.sleep())
+print('Check to see if monster makes a noise during scare attack:')
+print(monster1.scare_attack())
