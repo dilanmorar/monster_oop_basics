c7807f173f8a01698444300ce7fb3c5a4e5ac22f
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 09:31:00 +0000

    adding a skill

--- Monster_class.py
+++ Monster_class.py
@@ -1,13 +1,16 @@
-skills = ['shape shift', 'quiet', 'strong', 'smart', 'smelly']
-
 class Monster():
-    def __init__(self, name, colour, skills):
+    def __init__(self, name = 'jim', colour = 'red', skills = []):
         self.name = name
         self.colour = colour
         self.skills = skills
+
     def sleep(self):
         return 'zzz'
     def eat(self):
         return 'nom nom nom'
     def scare_attack(self):
         return 'Boo'
+
+    def add_skills(self, skill):
+        chosen_monster = self
+        chosen_monster.skills.append(skill)
--- Monster_class_runs.py
+++ Monster_class_runs.py
@@ -1,7 +1,14 @@
 from Monster_class import *
 
-monster1 = Monster('Mike', 'green', 'smart')
+monster1 = Monster('Mike', 'green', 'big.')
+
+print(monster1.name)
+print(monster1.colour)
+print(monster1.skills)
 
 print(monster1.eat())
 print(monster1.sleep())
 print(monster1.scare_attack())
+
+monster1. add_skills('invisible')
+print(monster1.skills)
